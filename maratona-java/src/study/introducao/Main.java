3c67970990bbcb175ae7723c088ef368f8c5824b
Pedro Bezerra <pedroh.bezerra02@gmail.com>
2025-07-08 14:43:40 -0300

    feat: aula 009 - comentários

--- maratona-java/src/study/introducao/Main.java
+++ maratona-java/src/study/introducao/Main.java
@@ -1,7 +1,19 @@
 package study.introducao;
 
+/**
+ * Cometário de Documentação {JavaDoc}
+ */
 public class Main {
     public static void main(String[] args ) {
+        //Comentário de uma linha
+
+        /*
+        Comentário
+        de
+        multiplas
+        linhas
+         */
+
         System.out.println("Hello, World!!");
     }
 }
